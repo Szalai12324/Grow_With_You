5dbe336f8ccbd1553aa5fb8bf9e034d1a8e34c63
Péter Varga <vargapeter389@gmail.com>
2026-05-18 11:33:24 +0200

    uze

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Routes, Route } from 'react-router-dom';
+import { Navigate, Routes, Route } from 'react-router-dom';
 import AdminPage from './mainPages/adminPage';
 import CustomerPage from './mainPages/customerPage';
 import DetailProduct from './customerPageComponents/detailProduct';
@@ -13,6 +13,7 @@ function App() {
     
     <div className="App">
       <Routes>
+        <Route path="/" element={<Navigate to="/customer" replace />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path='/customer' element={<CustomerPage />}/>
         <Route path="/product/:id" element={<DetailProduct />} />
